705e43e283490c743bc95bc35b6773c506a5aa8c
CurtisLV <curtislv@gmail.com>
2022-06-25 20:13:18 +0300

    Finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+// let randString = 'Animal loots foliated detail of stool lamina.';
 
+const palindromes = function (string) {
+    // lowerCase and remove random symbols
+    fixedString = string.toLowerCase().replace(/[^a-z]/g, '');
+
+    return (
+        fixedString
+            //split
+            .split('')
+            // reverse
+            .reverse()
+            //combine and check against same string
+            .join('') === fixedString
+    );
 };
-
+// console.log(palindromes(randString));
 // Do not edit below this line
 module.exports = palindromes;
